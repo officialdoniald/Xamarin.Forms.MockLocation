fdc0d24a5af5d695c000b33a21884327c855a7f9
Bence Lenart <bence960206@gmail.com>
2020-04-16 23:03:46 +0200

    Get Location

--- Xamarin.Forms.MockLocation.Mobile/Xamarin.Forms.MockLocation.Mobile/MainPage.xaml.cs
+++ Xamarin.Forms.MockLocation.Mobile/Xamarin.Forms.MockLocation.Mobile/MainPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Xamarin.Forms.MockLocation.Mobile
@@ -16,6 +18,27 @@ namespace Xamarin.Forms.MockLocation.Mobile
         public MainPage()
         {
             InitializeComponent();
-        }
+
+            Thread thread = new Thread(async () =>
+            {
+                while (true)
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                    var location = await Geolocation.GetLocationAsync(request);
+
+                    if (location != null)
+                    {
+                        if (location.IsFromMockProvider)
+                        {
+                            //Detect mocking location
+                        }
+                        else
+                        {
+                            //Detect normal location
+                        }
+                    }
+                }
+            });
+                    }
     }
 }
